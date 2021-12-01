84571a9992939fec9a29ef6a47a551770e26fdc6
Ray West <rwest025@fiu.edu>
2021-12-01 17:53:07 -0500

    Fixed port

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const app = require("express")();
-const PORT = 8080;
+const PORT = process.env.PORT || 8080;
 const appRoutes = require("./routes/apps");
 
 app.listen(PORT, () => console.log(`Listening on ${PORT}`));
